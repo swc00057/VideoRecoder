c1a8599afef6d6de26f47fe92bee64d111b2de72
swc00057 <swc00057@naver.com>
2022-10-11 03:16:34 +0900

    refactor: 프로젝트 디렉토리 구조 재정립

---
+++ VideoRecorder/PlayBack/PlayBackViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PlayBackViewController.swift
+//  VideoRecorder
+//
+//  Created by 신동원 on 2022/10/11.
+//
+
+import UIKit
+
+//영상 재생 화면
+class PlayBackViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
---
+++ VideoRecorder/RecodeVideoList/RecodeListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RecodeListViewController.swift
+//  VideoRecorder
+//
+//  Created by 신동원 on 2022/10/11.
+//
+
+import UIKit
+
+//녹화 영상 목록 화면
+class RecodeListViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
