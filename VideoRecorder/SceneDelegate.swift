98d53403492a32364bd07a3b7d6ccc66dffb5255
swc00057 <swc00057@naver.com>
2022-10-13 21:44:24 +0900

    fix: compile error

--- VideoRecorder/SceneDelegate.swift
+++ VideoRecorder/SceneDelegate.swift
@@ -14,7 +14,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        let mainVC = RecordingViewController()
+        let mainVC = RecodeListViewController()
         let navigationController = UINavigationController(rootViewController: mainVC)
         window?.rootViewController = navigationController
         window?.backgroundColor = .systemBackground
