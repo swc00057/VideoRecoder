0004f28935bad33735a2ad47fe68a39aa3cdf63c
betterhee <betterhee@gmail.com>
2022-10-14 15:12:31 +0900

    style: 코드 컨벤션 수정

--- VideoRecorder/RecodeVideoList/RecodeListViewController.swift
+++ VideoRecorder/RecodeVideoList/RecodeListViewController.swift
@@ -10,12 +10,21 @@ import Photos
 import AVKit
 
 final class RecodeListViewController: UIViewController {
-    
+
+    // MARK: Constants
+
+    enum Metrics {
+        static let rowHeight: CGFloat = 100
+        static let thumbnailSize: CGSize = .init(width: 100, height: 100)
+    }
+
+    // MARK: UI
+
     private lazy var tableView: UITableView = {
         let tableView = UITableView()
         tableView.translatesAutoresizingMaskIntoConstraints = false
         tableView.rowHeight = UITableView.automaticDimension
-        tableView.estimatedRowHeight = 100
+        tableView.estimatedRowHeight = Metrics.rowHeight
         tableView.register(RecodeListCell.self, forCellReuseIdentifier: RecodeListCell.reuseIdentifier)
         tableView.delegate = self
         tableView.dataSource = self
@@ -33,7 +42,7 @@ final class RecodeListViewController: UIViewController {
         return button
     }()
     
-    private lazy var activityIndicator: UIActivityIndicatorView = {
+    private let activityIndicator: UIActivityIndicatorView = {
         let indicator = UIActivityIndicatorView(style: .large)
         indicator.translatesAutoresizingMaskIntoConstraints = false
         indicator.hidesWhenStopped = true
@@ -41,23 +50,27 @@ final class RecodeListViewController: UIViewController {
         return indicator
     }()
     
-    private let miniPlayerViewController: UIViewController = {
-        let controller = UIViewController()
-        return controller
-    }()
-    
+    private let miniPlayerViewController = UIViewController()
+
+    // MARK: Properties
+
+    // Pagination
+    private let fetchSize = 6
+    private var fetchLimit = 0
+    private var isFetching: Bool = false
+
+    // Fetch Videos
     private let imageManager = PHCachingImageManager()
     private let albumTitle = "MyVideos"
-    private let fetchSize = 6
-    
-    private var fetchLimit = 0
     private var fetchResult: PHFetchResult<PHAsset>?
-    private var isFetching: Bool = false
     private var album: PHAssetCollection?
 
+    // Preview
     private var looper: AVPlayerLooper?
     private var queuePlayer: AVQueuePlayer?
-    
+
+    // MARK: View Life Cycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -71,25 +84,11 @@ final class RecodeListViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        print("1번뷰 register")
+
         PHPhotoLibrary.shared().register(self)
     }
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        print("1번뷰 appear")
-    }
-    
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        guard let album = album else { return }
 
-        let offsetY = tableView.contentOffset.y
-        let contentHeight = tableView.contentSize.height
-        let height = tableView.bounds.size.height
-        
-        if offsetY > contentHeight - height {
-            fetchMoreAssets(in: album)
-        }
-    }
+    // MARK: Layout
     
     private func setupNavigation() {
         navigationItem.title = "Video List"
@@ -113,6 +112,19 @@ final class RecodeListViewController: UIViewController {
         ])
     }
 
+    // MARK: Pagination & Fetch Videos
+
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        guard let album = album else { return }
+
+        let offsetY = tableView.contentOffset.y
+        let contentHeight = tableView.contentSize.height
+        let height = tableView.bounds.size.height
+
+        if offsetY > contentHeight - height {
+            fetchMoreAssets(in: album)
+        }
+    }
 
     /// Fetching Asset Collection for specific `albumTitle`
     private func fetchAssetCollection(
@@ -155,28 +167,56 @@ final class RecodeListViewController: UIViewController {
         fetchResult = PHAsset.fetchAssets(in: assetCollection, options: options)
         completion?()
     }
-    
-    @objc
-    private func recordButtonClicked() {
-        // TODO: 영상녹화화면으로 이동
-        let vc = RecordingViewController()
-        guard navigationController?.topViewController == self else { return }
-        self.navigationController?.pushViewController(vc, animated: true)
-        //        vc.modalPresentationStyle = .overFullScreen
-        //        self.present(vc, animated: true)
+
+    // MARK: Preview
+
+    private func setupPlayer(_ asset: PHAsset, completion: @escaping (AVAsset)->() ) {
+        PHCachingImageManager().requestAVAsset(forVideo: asset, options: nil) { avAsset, _, _ in
+            guard let avAsset = avAsset else { return }
+            completion(avAsset)
+        }
+    }
+
+    private func setupMiniPlayer(_ asset: PHAsset ) {
+        PHCachingImageManager().requestAVAsset(forVideo: asset, options: nil) { avAsset, _, _ in
+            guard let avAsset = avAsset else { return }
+
+            DispatchQueue.main.async {
+                let playerItem = AVPlayerItem(asset: avAsset)
+                self.queuePlayer = AVQueuePlayer(items: [playerItem])
+                self.looper = AVPlayerLooper(player: self.queuePlayer!, templateItem: playerItem, timeRange: self.createCMTimeRange(start: 0, end: 3000))
+
+                let playerLayer = AVPlayerLayer(player: self.queuePlayer)
+                playerLayer.videoGravity = .resizeAspectFill
+                self.miniPlayerViewController.view.layer.addSublayer(playerLayer)
+                playerLayer.frame = self.miniPlayerViewController.view.frame
+
+                self.queuePlayer!.play()
+            }
+        }
     }
     
-    func createCMTimeRange(start:Int64, end:Int64) -> CMTimeRange {
-
+    private func createCMTimeRange(start: Int64, end: Int64) -> CMTimeRange {
         let a: CMTime = CMTime(value: start, timescale: 600)
         let b: CMTime = CMTime(value: end, timescale: 600)
         return CMTimeRange(start: a, end: b)
     }
+
+    // MARK: Action Handler
+
+    @objc
+    private func recordButtonClicked() {
+        let vc = RecordingViewController()
+        guard navigationController?.topViewController == self else { return }
+        navigationController?.pushViewController(vc, animated: true)
+    }
+
 }
 
 // MARK: - UITableViewDataSource
 
 extension RecodeListViewController: UITableViewDataSource {
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         fetchResult?.count ?? 0
     }
@@ -188,53 +228,25 @@ extension RecodeListViewController: UITableViewDataSource {
         guard let asset = fetchResult?.object(at: indexPath.row) else { return cell }
 
         cell.configure(with: asset)
-        let size = CGSize(width: 100, height: 100)
-        imageManager.requestImage(for: asset, targetSize: size, contentMode: .aspectFill, options: nil) { image, _ in
+        imageManager.requestImage(for: asset, targetSize: Metrics.thumbnailSize, contentMode: .aspectFill, options: nil) { image, _ in
             if cell.representedAssetIdentifier == asset.localIdentifier {
                 cell.thumbnailImageView.image = image
             }
         }
         return cell
     }
+
 }
 
 // MARK: - UITableViewDelegate
 
 extension RecodeListViewController: UITableViewDelegate {
-    
-    func setupPlayer(_ asset: PHAsset, completion: @escaping (AVAsset)->() ) {
-        
-        
-        PHCachingImageManager().requestAVAsset(forVideo: asset, options: nil) { avAsset, _, _ in
-            guard let avAsset = avAsset else { return }
-            completion(avAsset)
-        }
-    }
-    
-    func setupMiniPlayer(_ asset: PHAsset ) {
-        
-        
-        PHCachingImageManager().requestAVAsset(forVideo: asset, options: nil) { avAsset, _, _ in
-            guard let avAsset = avAsset else { return }
-            DispatchQueue.main.async {
-                let playerItem = AVPlayerItem(asset: avAsset)
-                self.queuePlayer = AVQueuePlayer(items: [playerItem])
-                self.looper = AVPlayerLooper(player: self.queuePlayer!, templateItem: playerItem, timeRange: self.createCMTimeRange(start: 0, end: 3000))
-                
-                let playerLayer = AVPlayerLayer(player: self.queuePlayer)
-                playerLayer.videoGravity = .resizeAspectFill
-                self.miniPlayerViewController.view.layer.addSublayer(playerLayer)
-                playerLayer.frame = self.miniPlayerViewController.view.frame
-                
-                self.queuePlayer!.play()
-            }
-            
-        }
-    }
-    
+
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: false)
+
         guard let asset = fetchResult?.object(at: indexPath.row) else { return }
+
         let fileName = asset.originalFilename
         setupPlayer(asset) { asset in
             DispatchQueue.main.async {
@@ -248,8 +260,8 @@ extension RecodeListViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
         guard let asset = fetchResult?.object(at: indexPath.row) else { return nil }
+
         let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { _, _, _ in
-            // TODO: 백업된 영상도 삭제합니다.
             PHPhotoLibrary.shared().performChanges({
                 PHAssetChangeRequest.deleteAssets([asset] as NSArray)
             }) { success, error in
@@ -269,6 +281,7 @@ extension RecodeListViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, contextMenuConfigurationForRowAt indexPath: IndexPath, point: CGPoint) -> UIContextMenuConfiguration? {
         guard let asset = fetchResult?.object(at: indexPath.row) else { return nil }
+
         setupMiniPlayer(asset)
         var configuration: UIContextMenuConfiguration!
         let assetIdentifier = asset.localIdentifier as NSCopying
@@ -281,56 +294,23 @@ extension RecodeListViewController: UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, willEndContextMenuInteraction configuration: UIContextMenuConfiguration, animator: UIContextMenuInteractionAnimating?) {
-        self.queuePlayer?.pause()
+        queuePlayer?.pause()
     }
+
 }
 
 // MARK: - PHPhotoLibraryChangeObserver
 
 extension RecodeListViewController: PHPhotoLibraryChangeObserver {
-    func photoLibraryDidChange(_ changeInstance: PHChange) {
-        DispatchQueue.main.sync {
-            guard let fetchResult = self.fetchResult,
-                  let details = changeInstance.changeDetails(for: fetchResult) else { return }
 
-            self.fetchResult = details.fetchResultAfterChanges
+    func photoLibraryDidChange(_ changeInstance: PHChange) {
+        guard let fetchResult = fetchResult,
+              let details = changeInstance.changeDetails(for: fetchResult) else { return }
+
+        self.fetchResult = details.fetchResultAfterChanges
+        DispatchQueue.main.async {
             self.tableView.reloadData()
         }
     }
-}
 
-//extension RecodeListViewController: viewLifeCycleDelegate {
-//    func notification() {
-//        self.queuePlayer!.pause()
-//    }
-//}
-//
-//protocol viewLifeCycleDelegate {
-//
-//    func notification()
-//}
-//
-//
-//class MiniViewController: UIViewController {
-//
-//    var delegate: viewLifeCycleDelegate?
-//
-//    init(){
-//        super.init(nibName: nil, bundle: nil)
-//    }
-//
-//    required init?(coder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//    }
-//    override func viewWillDisappear(_ animated: Bool) {
-//        super.viewWillDisappear(animated)
-//        delegate?.notification()
-//        self.view.layer.sublayers?.forEach {
-//            $0.removeFromSuperlayer()
-//        }
-//    }
-//}
+}
